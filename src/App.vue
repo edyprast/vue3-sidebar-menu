d68f6293cf0037bc5e0c1595afe4d3f6dde4788e
edyprast <edprast74@gmail.com>
2024-03-06 19:14:19 +0700

    Changes to be committed: 	modified:   src/App.vue 	new file:   src/assets/images/logo_evi.png 	new file:   src/components/bs5_loginpage/loginpage.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script>
 // import asmr_side_menu from './components/asmr_prog2/sidemenu1.vue';
-import bs_side_menu from './components/bs5_sidemenu/bs_sidemenu1.vue';
+// import bs_side_menu from './components/bs5_sidemenu/bs_sidemenu1.vue';
+import login_page from './components/bs5_loginpage/loginpage.vue';
+
 export default {
-  components: {bs_side_menu},
+  components: {login_page},
   setup() {
 
   }
@@ -11,7 +13,8 @@ export default {
 
 <template>
 <!-- <asmr_side_menu/>-->
-  <bs_side_menu/>
+ <!-- <bs_side_menu/> -->
+<login_page/>
 </template>
 
 <style>
---
+++ src/components/bs5_loginpage/loginpage.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-sm-center h-100">
+				<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+					<div class="text-center my-5">
+						<img src="../../assets/images/logo_evi.png" alt="logo" width="100">
+					</div>
+					<div class="card shadow-lg">
+						<div class="card-body p-5">
+							<h1 class="fs-4 card-title fw-bold mb-4">Login</h1>
+							<form method="POST" class="needs-validation" novalidate="" autocomplete="off">
+								<div class="mb-3">
+									<label class="mb-2 text-muted" for="email">E-Mail Address</label>
+									<input id="email" type="email" class="form-control" name="email" value="" required autofocus>
+									<div class="invalid-feedback">
+										Email is invalid
+									</div>
+								</div>
+
+								<div class="mb-3">
+									<div class="mb-2 w-100">
+										<label class="text-muted" for="password">Password</label>
+										<a href="forgot.html" class="float-end">
+											Forgot Password?
+										</a>
+									</div>
+									<input id="password" type="password" class="form-control" name="password" required>
+								    <div class="invalid-feedback">
+								    	Password is required
+							    	</div>
+								</div>
+
+								<div class="d-flex align-items-center">
+									<div class="form-check">
+										<input type="checkbox" name="remember" id="remember" class="form-check-input">
+										<label for="remember" class="form-check-label">Remember Me</label>
+									</div>
+									<button type="submit" class="btn btn-primary ms-auto">
+										Login
+									</button>
+								</div>
+							</form>
+						</div>
+						<div class="card-footer py-3 border-0">
+							<div class="text-center">
+								Don't have an account? <a href="register.html" class="text-dark">Create One</a>
+							</div>
+						</div>
+					</div>
+					<div class="text-center mt-5 text-muted" style="font-size: 0.75rem;">
+						Copyright &copy; 2023 &mdash; Badan Pengawasan Keuangan dan Pembangunan 
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
